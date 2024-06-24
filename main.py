22297bf015bc6239757a0a0ca168ebd3ea871436
Florencia Lihuel Zagaglia Helesic <florenciazh@gmail.com>
2024-06-24 07:05:31 -0300

    editmain.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hola mundo")
+#papa crema sal pimienta huevo Nuez moscada Manteca queso mantecoso 
+# albahaca tomate aceite carne oregano salsa de tomate ajo vino
+#harina queso rallado cebolla pan rallado
+listaDeing=["papa","crema","sal","pimienta","huevo","nuez moscada","manteca","queso mantecoso","albahaca","tomate","aceite","carne","oregano","salsa de tomate","ajo","vino","harina","queso rallado","cebolla","pan rallado"]
+for n in listaDeing:
+    print(n)
+
+milHojasdepapa=["250g de papa","50cc de crema","5g de sal","5g de pimienta","2 huevos","5g de nuez moscada","100g de manteca"]
+omeletteDequeso=["150g de queso mantecoso","200g de tomate","250g de papa","5g de sal","5g de pimienta","5g de albahaca","50cc de aceite"]
+carneAlhorno=["150g de carne","300g de papas","200g de tomate","5g de oregano","5g de sal","50cc de aceite"]
+noquis=["200g de salsa de tomate","1 diente de ajo","50cc de vino tinto","200g de papa","100g de queso rallado","50cc de aceite","100g de harina","1 huevo"]
+bombasDepapa=["1/2 cebolla","300g de papa","200g de tomate","1 huevo","90g de harina","100g de pan rallado","50cc de aceite"]
+listaDecomidas=[["milHojasdepapa"],["omeletteDequeso"],["carneAlhorno"],["noquis"],["bombasDepapa"]]
